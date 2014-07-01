9016d0f64fe6678ff81ee4724315883f98ad0fd4
by90 <11084184@qq.com>
2014-07-01 16:39:19 +0800

    add dad file for test

--- test/gtest_main.cc
+++ test/gtest_main.cc
@@ -5,6 +5,10 @@ GTEST_API_ int main(int argc, char **argv) {
 	printf("Running main() from gtest_main.cc\n");
 	testing::InitGoogleTest(&argc, argv);
 	int result=RUN_ALL_TESTS();
-	system("PAUSE");
+	
+	//如果命令行方式运行，则在此暂停，以便观察命令窗口
+	//如果在测试资源管理器运行，则要注释掉，否则测试会一直运行并等待。
+	//system("PAUSE");
+	
 	return result;
 }
