13ba6330e29adf38dfb8b40b0d42618640c7ab65
by90 <11084184@qq.com>
2014-07-06 16:56:21 +0800

    config test and model project for using sqlite3.lib.

--- model/db.h
+++ model/db.h
@@ -1,5 +1,7 @@
 #include <string>
 #include <codecvt>
+#include <fstream>
+#include "sqlite/sqlite3.h"
 #ifndef db_h
 #define db_h
 class db
@@ -9,6 +11,17 @@ public:
 	static void set_default(std::string _default)
 	{
 		default_ = _default;
+
+		//判断文件是否存在，fstream的方法仍然是打开是否正常
+		//因此直接略过，用数据库是否正常打开，来判断。
+		sqlite3 *db = NULL;
+		int rc = 0;
+		rc = sqlite3_open_v2(default_.c_str(), &db, SQLITE_OPEN_READWRITE , NULL);
+		if (rc != SQLITE_OK)
+		{
+			sqlite3_close_v2(db);
+		}
+
 	}
 	static void set_default(std::wstring _default)
 	{
