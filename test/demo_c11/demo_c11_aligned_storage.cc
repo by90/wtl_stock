c3fc0e814ebc4d1bb76b9818031271c91fd6406e
by90 <11084184@qq.com>
2014-09-06 02:55:14 +0800

    add test file for unordered_map.

---
+++ test/demo_c11/demo_c11_aligned_storage.cc
@@ -0,0 +1,6 @@
+#include "gtest/gtest.h"
+#include <type_traits>
+TEST(DemoC11, aligned_storage)
+{
+	//std::aligned_storage
+}
---
+++ test/demo_c11/demo_c11_unordered_map.cc
@@ -0,0 +1,10 @@
+#include <unordered_map>
+#include <concurrent_unordered_map.h>
+#include <unordered_set>
+#include "gtest/gtest.h"
+using namespace std;
+
+TEST(DemoC11, unordered_map)
+{
+
+}
